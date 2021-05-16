1df2ce0ae8f34a583eff219a59da88dac5995ed1
Ethan Shealey <ethan.shealey@gmail.com>
2021-05-16 09:53:38 -0400

    added ability to specify difficulty and subject

--- TriviaBot.py
+++ TriviaBot.py
@@ -26,12 +26,30 @@ async def on_message(msg):
         if len(content) == 1 and content[0] == '?help':
             # help prompt
             embed = discord.Embed(title='Help', color=0x1FB3FC)
-            embed.add_field(name='Welcome to TriviaBot!', value='How to use:\n• To play: type `?triv` in chat\n• To respond: TriviaBot will automatically react with the possible options, click on the option you think is correct!', inline=False)
+            embed.add_field(name='Welcome to TriviaBot!', value='How to use:\n• To play: type `?triv` in chat\n• OPTIONAL: You can also specify a subject ID by doing `?triv <subject id>`\n• OPTIONAL: You can also specify difficulty using one of the three keywords, i.e `?triv hard`\n• OPTIONAL: Finally, you can combine both these in any order, i.e `?triv 10 hard` or `?triv hard 10`\n• To respond: TriviaBot will automatically react with the possible options, click on the option you think is correct!\n\nSubject IDs:\n1) General Knowledge\n2) Entertainment: Books\n3) Entertainment: Film\n4) Entertainment: Music\n5) Entertainment: Musicals & Theatres\n6) Entertainment: Television\n7) Entertainment: Video Games\n8) Entertainment: Board Games\n9) Science & Nature\n10) Science: Computers\n11) Science: Mathematics\n12) Mythology\n13) Sports\n14) Geography\n15) History\n16) Politics\n17) Art\n18) Celebrities\n19) Animals\n20) Vehicles\n21) Entertainment: Comics\n22) Science: Gadgets\n23) Entertainment: Japanese Anime & Manga\n24) Entertainment: Cartoon & Animations\n\nDifficulties:\n• easy\n• medium\n• hard', inline=False)
             await msg.channel.send(embed=embed)
 
-        elif len(content) == 1 and content[0] == '?triv':
-            # get data
-            url = 'https://opentdb.com/api.php?amount=1'
+        elif len(content) >= 1 and content[0] == '?triv':
+            # get data                    
+            url = 'https://opentdb.com/api.php?amount=1' #+ (('&category=' + str(int(content[1])+8)) if len(content) > 1 else '')
+            
+            if len(content) == 2:
+                if content[1].isdigit():
+                    url += '&category=' + str(int(content[1]) + 8)
+                if content[1] == 'easy' or content[1] == 'medium' or content[1] == 'hard':
+                    url += '&difficulty=' + content[1]
+
+            elif len(content) == 3:
+                if content[1].isdigit():
+                    url += '&category=' + str(int(content[1]) + 8)
+                elif content[2].isdigit():
+                    url += '&category=' + str(int(content[2]) + 8)
+
+                if content[1] == 'easy' or content[1] == 'medium' or content[1] == 'hard':
+                    url += '&difficulty=' + content[1]
+                elif content[2] == 'easy' or content[2] == 'medium' or content[2] == 'hard':
+                    url += '&difficulty=' + content[2]
+
             data = json.loads(urlopen(url).read().decode("utf-8"))
 
             #parse data
@@ -52,7 +70,7 @@ async def on_message(msg):
                 random.shuffle(possible_ans)
 
             # create embed
-            embed = discord.Embed(title="Welcome to Trivia!", color=0x1FB3FC)
+            embed = discord.Embed(title="Welcome to Trivia!", color=0x46e065 if diff == 'easy' else 0xf2f549 if diff == 'medium' else 0xfc4b30)
             embed.add_field(name='Category', value=cat, inline=False)
             embed.add_field(name='Difficulty', value=diff, inline=False)
             if not type == 'boolean':
@@ -107,15 +125,5 @@ async def on_message(msg):
             else:
                 embed = discord.Embed(title="Incorrect!", description=f"Sorry! The correct answer was {correct_ans}", color=0xFC1F4E)       
             await msg.channel.send(embed=embed)
-
-            print(correct_ans, possible_ans[ans])
-
-        '''
-        elif len(content) == 2 and content[0] == '?triv' and content[1] == 'adv':
-            await msg.channel.send('Here is the prompt for an advanced question!')
-
-        elif len(content) == 4 and content[0] == '?triv' and content[1] == 'adv':
-            await msg.channel.send('Here is an advanced question!')
-        '''
     
 client.run(TOKEN)
